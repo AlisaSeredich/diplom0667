15a7d681077dcdc93b616725c19b29c46d65309d
AlisaSeredich <109818565+AlisaSeredich@users.noreply.github.com>
2022-09-05 20:13:10 +0300

    Updane sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -2,7 +2,7 @@
 $userName = $_POST["user_name"];
 $contact = $_POST["contact"];
 $textmail = $_POST["textmail"];
-echo "$userName, $contact, $textmail";
+echo "Ваша заявка принята";
 mail('bootx64@mail.ru', 'Новая заявка', "$userName, $contact, $textmail");
 
 ?>
